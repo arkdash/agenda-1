9c2ca158aa9800b733cf348b80a120e355485804
alaudegp <christopher.ferreira.silva@outlook.com>
2019-03-21 21:36:13 -0300

    Criado um novo método na classe Service e um botão no formulário.

--- src/com/agenda/service/CadastraUsuarioService.java
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -6,10 +6,6 @@ public class CadastraUsuarioService {
 
 	public void cadastra(Pessoa pessoa) {
 		
-		System.out.println(pessoa.getNome());
-		System.out.println(pessoa.getEmail());
-		System.out.println(pessoa.getEndereco());
-		System.out.println(pessoa.getTelefone());
 		
 		
 	}
