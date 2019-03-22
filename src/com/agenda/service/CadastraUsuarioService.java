6baa3e8100b4575c1c108bca765941af4db2ff89
alaudegp <christopher.ferreira.silva@outlook.com>
2019-03-21 21:33:40 -0300

    Criei um novo método na classe Service e um botão no formulário.

--- src/com/agenda/service/CadastraUsuarioService.java
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -1,5 +1,17 @@
 package com.agenda.service;
 
+import com.agenda.model.Pessoa;
+
 public class CadastraUsuarioService {
+
+	public void cadastra(Pessoa pessoa) {
+		
+		System.out.println(pessoa.getNome());
+		System.out.println(pessoa.getEmail());
+		System.out.println(pessoa.getEndereco());
+		System.out.println(pessoa.getTelefone());
+		
+		
+	}
 
 }
