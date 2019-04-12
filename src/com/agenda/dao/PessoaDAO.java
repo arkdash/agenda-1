0f8998ff8dcd8e90b65b50187d9b8e2de84dd825
Vinicius0li <vinicius.oliveira524@etec.gov.br>
2019-04-11 21:39:20 -0300

    Criamos um Server

--- src/com/agenda/dao/PessoaDAO.java
+++ src/com/agenda/dao/PessoaDAO.java
@@ -1,12 +1,36 @@
 package com.agenda.dao;
 
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.agenda.model.Pessoa;
+import com.agenda.util.ConnectionFactory;
 import com.mysql.jdbc.Connection;
 
 public class PessoaDAO {
-	
+
+	private Connection connection;
+
 	public void cadastro(Pessoa pessoa) {
+
 		String SQL = "insert into pessoas (nome, email, endereco, telefone) values (?,?,?,?)";
-	}}
+
+		try {
+			this.connection = new ConnectionFactory().getConnection();
+			PreparedStatement stmt = this.connection.clientPrepareStatement(SQL);
+
+			stmt.setString(1, pessoa.getNome());
+			stmt.setString(2, pessoa.getEmail());
+			stmt.setString(3, pessoa.getEndereco());
+			stmt.setString(4, pessoa.getTelefone());
+
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException e) {
+
+			throw new RuntimeException(e);
+		}
+
+	}
+}
--- src/com/agenda/util/ConnectionFactory.java
+++ src/com/agenda/util/ConnectionFactory.java
@@ -11,7 +11,7 @@ public class ConnectionFactory {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 				return (Connection) DriverManager
-						.getConnection("jdbc:mysql://localhost/agendadb", "root", "");
+						.getConnection("jdbc:mysql://localhost/agendabd", "root", "");
 			} catch (ClassNotFoundException e) {
 				throw new RuntimeException(e);
 			}
